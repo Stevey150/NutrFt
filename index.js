8a69a79287f1efc4b34983cb3ef216a6a6cd9b01
Luis Martinez Ferraez <lemartinezfrz@gmail.com>
2024-12-25 02:15:07 -0500

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const { createClient } = require('@supabase/supabase-js');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Supabase client setup
 
